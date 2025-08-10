b5a3702ba0e2529753dbd6439e843852a82ed565
Engin Bolat <engi.bolat@gmail.com>
2025-08-11 01:36:17 +0300

    feat: added icons

---
+++ AirbnbClone/Constants/Enum/AppIcon.swift
@@ -0,0 +1,83 @@
+//
+//  AppIcon.swift
+//  AirbnbClone
+//
+//  Created by Engin on 11.08.2025.
+//
+
+import Foundation
+
+/// TODO:
+/// Bazı ikonların arka planları beyaz kalmış durumda onları düzeltmek gerekli.
+
+
+enum AppIcon: CaseIterable {
+    case OutlineCar
+    case OutlineLock
+    case OutlineLocation
+    case OutlineSearch
+    case OutlineMessage
+    case OutlineUser
+    case OutlineBlock
+    case OutlineHeart
+    case OutlineBriefcase
+    case OutlineMenu
+    case OutlineCalendar
+    case OutlineCalendar2
+    case OutlineClose
+    case OutlinePlus
+    case OutlineMinus
+    case OutlineChevronLeft
+    case OutlineChevronRight
+    case OutlineArrowLeft
+    case OutlineArrowRight
+    case OutlineBell
+    case OutlineFilter
+    case OutlineTranslate
+    case OutlineBed
+    case OutlineDoor
+    case OutlineDoorLock
+    case OutlineDownload
+    case OutlineMoney
+    case OutlineView
+    case OutlineView2
+    case OutlineView3
+    case OutlineKitchen
+    case OutlineHairDryer
+    case OutlineShampoo
+    case OutlineHotWater
+    case OutlineShowerGel
+    case OutlineWasher
+    case OutlineNoStairs
+    case OutlineKey
+    case OutlineNoKitchen
+    case OutlineNoAC
+    case OutlineNoMonoxide
+    case OutlineNoEntrance
+    case OutlineCCTV
+    case OutlineMonixide
+    case OutlineWifi
+    case OutlineBeach
+    case OutlineBeach2
+    case OutlineUFO
+    case OutlinePool
+    case OutlineIsland
+    case OutlineArctic
+    case OutlineCabin
+    case OutlineRoom
+    case OutlineWorkspace
+    case OutlineCaves
+    case OutlineAirbnb
+    
+    //MARK: Filled
+    case FilledVerification
+    case FilledFlag
+    case FilledAdd
+    case FilledHome
+    case FilledMaps
+    case FilledStar
+    
+    //MARK: TwoTone
+    case TwoToneEarnMoney
+    case TwoToneAirbnbProject
+}
